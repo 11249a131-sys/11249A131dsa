7df6152e92b417712277a476c7e118c600850aa8
11249a131-sys <11249a131@kanchiuniv.ac.in>
2026-02-10 12:40:57 +0530

    Add files via upload

---
+++ towerofhanoi.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void towerofhanoi(int n,char s,char d, char t)
+{
+	if(n==1)
+	{
+		printf("move the disk %d from %c to %c",n,s,d);
+		return;
+	}
+	towerofhanoi(n-1,s,t,d);
+	printf("\n move disk %d from %c to %c",n,s,d);
+	towerofhanoi(n,s,d,t);
+	printf("\n move disk %d from %c to %c",n,s,d);
+	towerofhanoi(n-1,t,d,s);
+}
+int main()
+{
+	int n;
+	char s,d,t;
+	printf("enter number of disks:");
+	scanf("%d",&n);
+	towerofhanoi(n,'s','d','t');
+}
